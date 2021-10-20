37e32553edd6d03b54cd54de0a04a868cc8b99f6
mherczku <61518890+mherczku@users.noreply.github.com>
2021-10-21 00:43:53 +0200

    Add uploadFromGalery feature to AddDialog, TODO Size it too big to display

--- app/src/main/java/hu/hm/icguide/ui/add/AddDialog.kt
+++ app/src/main/java/hu/hm/icguide/ui/add/AddDialog.kt
@@ -1,19 +1,27 @@
 package hu.hm.icguide.ui.add
 
+import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.annotation.StringRes
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import co.zsmb.rainbowcake.navigation.navigator
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
@@ -21,6 +29,7 @@ import com.google.firebase.firestore.GeoPoint
 import dagger.hilt.android.AndroidEntryPoint
 import hu.hm.icguide.R
 import hu.hm.icguide.databinding.DialogAddBinding
+import hu.hm.icguide.extensions.toast
 import hu.hm.icguide.extensions.validateNonEmpty
 import hu.hm.icguide.interactors.FirebaseInteractor
 import java.io.ByteArrayOutputStream
@@ -44,7 +53,12 @@ class AddDialog(private val position: LatLng) : DialogFragment(),
     private lateinit var binding: DialogAddBinding
     private lateinit var startForResult: ActivityResultLauncher<Intent>
     private var imageSet = false
+    private lateinit var startForResultGalery: ActivityResultLauncher<Intent>
+    private var isPermissionGranted = false
+    private lateinit var permReqLauncher: ActivityResultLauncher<String>
 
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
         binding = DialogAddBinding.inflate(LayoutInflater.from(context))
@@ -53,6 +67,17 @@ class AddDialog(private val position: LatLng) : DialogFragment(),
                 if (result.resultCode == Activity.RESULT_OK) {
                     val imageBitmap = result.data?.extras?.get("data") as Bitmap
                     imageSet = true
+                    binding.imgAttach.setImageBitmap(imageBitmap)
+                }
+            }
+        startForResultGalery =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val uri = result.data?.data
+                    uri ?: return@registerForActivityResult
+                    val source = ImageDecoder.createSource(requireActivity().contentResolver, uri)
+                    val imageBitmap = ImageDecoder.decodeBitmap(source)
+                    //TODO hogy beleférjen mert most tul nagy
                     binding.imgAttach.setImageBitmap(imageBitmap)
                 }
             }
@@ -66,7 +91,18 @@ class AddDialog(private val position: LatLng) : DialogFragment(),
 
     private fun setupViews() {
         binding.imgAttach.setOnClickListener {
-            attachImage()
+            AlertDialog.Builder(context)
+                .setCancelable(true)
+                .setMessage(R.string.galery_or_create)
+                .setPositiveButton(
+                    getString(R.string.take_photo)
+                ) { _, _ ->
+                    attachImage()
+                }
+                .setNeutralButton(getString(R.string.upload_photo)) { _, _ ->
+                    handleStoragePermission()
+                }
+                .create().show()
         }
         binding.btnAdd.setOnClickListener { addClick() }
     }
@@ -82,7 +118,8 @@ class AddDialog(private val position: LatLng) : DialogFragment(),
         }
 
         val newShop = UploadShop(
-            name = binding.addNameTextField.editText?.text.toString().replaceFirstChar { it.uppercase() },
+            name = binding.addNameTextField.editText?.text.toString()
+                .replaceFirstChar { it.uppercase() },
             address = binding.addAddressTextField.editText?.text.toString(),
             geoPoint = GeoPoint(position.latitude, position.longitude),
             rate = 0F,
@@ -101,10 +138,6 @@ class AddDialog(private val position: LatLng) : DialogFragment(),
                 toast(e.message)
             }
         }
-    }
-
-    private fun toast(message: String?) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun validateForm(): Boolean {
@@ -151,6 +184,59 @@ class AddDialog(private val position: LatLng) : DialogFragment(),
         toast(getString(R.string.add_unsuccessful))
         toast(p0.localizedMessage)
         this.dismiss()
+    }
+
+    private fun pickImage() {
+        val getPicIntent = Intent(Intent.ACTION_PICK)
+        getPicIntent.type = "image/*"
+        startForResultGalery.launch(Intent(getPicIntent))
+    }
+
+    private fun showRationaleDialog(
+        @StringRes title: Int = R.string.rationale_dialog_title,
+        @StringRes explanation: Int,
+        onPositiveButton: () -> Unit,
+        onNegativeButton: () -> Boolean? = { navigator?.pop() }
+    ) {
+        val alertDialog = AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(explanation)
+            .setCancelable(false)
+            .setPositiveButton(R.string.proceed) { dialog, _ ->
+                dialog.cancel()
+                onPositiveButton()
+            }
+            .setNegativeButton(R.string.exit) { _, _ -> onNegativeButton() }
+            .create()
+        alertDialog.show()
+    }
+
+    private fun handleStoragePermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                showRationaleDialog(
+                    explanation = R.string.external_storage_explanation,
+                    onPositiveButton = this::requestStoragePermission
+                )
+            } else {
+                requestStoragePermission()
+            }
+        } else {
+            isPermissionGranted = true
+            pickImage()
+        }
+    }
+
+    private fun requestStoragePermission() {
+        permReqLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
     }
 
 }
